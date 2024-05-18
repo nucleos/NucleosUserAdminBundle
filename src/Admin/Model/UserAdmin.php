4d79f8d79bce5dd89efbb57178e506fd55623415
core23 <mail@core23.de>
2024-05-18 18:56:44 +0200

    Add status to show page

--- src/Admin/Model/UserAdmin.php
+++ src/Admin/Model/UserAdmin.php
@@ -22,6 +22,7 @@ use Nucleos\UserBundle\Util\UserManipulator;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
 use Sonata\AdminBundle\Show\ShowMapper;
@@ -129,6 +130,7 @@ abstract class UserAdmin extends AbstractAdmin
             ->with('general', ['label' => 'show.group_general'])
                 ->add('username')
                 ->add('email')
+                ->add('enabled', FieldDescriptionInterface::TYPE_BOOLEAN)
             ->end()
             ->with('groups', ['label' => 'show.group_groups'])
                 ->add('groups')
