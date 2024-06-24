6a08ae5a4be1c6175120122b86da785c3a347ab6
core23 <mail@core23.de>
2024-06-24 20:41:59 +0200

    Fix symfony deprecations

--- src/DependencyInjection/NucleosUserAdminExtension.php
+++ src/DependencyInjection/NucleosUserAdminExtension.php
@@ -20,9 +20,9 @@ use RuntimeException;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
 final class NucleosUserAdminExtension extends Extension implements PrependExtensionInterface
